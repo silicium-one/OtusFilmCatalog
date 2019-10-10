3a6d35ea02b65c0cb419f72b57340785eae61207
Antonyj Malin (Ramax home virtual PC) <silicium_one@mail.ru>
2019-10-10 19:51:43 +0300

    FIX: потеря выделения выбранного фильма

--- OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/ui/MainActivity.java
+++ OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/ui/MainActivity.java
@@ -71,6 +71,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 gotoDetailActivity();
             }}))
             root.addView(v);
+
+        setSelectedFilmTag(selectedFilmTag);
     }
 
     private void aboutMode() {
