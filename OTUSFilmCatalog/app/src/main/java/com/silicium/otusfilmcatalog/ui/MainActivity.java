bcc50ea976c0f55bbe5badeb0a0b50f5c3f67e85
Antonyj Malin (Ramax home virtual PC) <silicium_one@mail.ru>
2019-10-10 15:31:26 +0300

    IMPR: переделка на Navigation Drawer

--- OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/ui/MainActivity.java
+++ OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.silicium.otusfilmcatalog.ui;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_activity_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_main_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         final LinearLayout root = findViewById(R.id.film_root_layout);
 
         FilmDescriptionStorage instance = FilmDescriptionStorage.getInstance();
