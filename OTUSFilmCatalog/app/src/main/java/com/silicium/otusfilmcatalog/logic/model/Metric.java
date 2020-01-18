af040c37f93f7f461e1d847c0db9738812345441
Antonyj Malin (Ramax home virtual PC) <silicium_one@mail.ru>
2020-01-18 23:24:34 +0300

    FIX: падение: Integer это не int

--- OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/logic/model/Metric.java
+++ OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/logic/model/Metric.java
@@ -2,7 +2,7 @@ package com.silicium.otusfilmcatalog.logic.model;
 
 public class Metric {
     public final String metricTag;
-    public Integer value;
+    public int value;
 
     public Metric(String metricTag) {
         this.metricTag = metricTag;
