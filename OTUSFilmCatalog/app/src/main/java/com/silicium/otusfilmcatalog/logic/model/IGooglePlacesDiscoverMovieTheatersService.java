8c450107a352f54f35e7039361c8d35f0e452a26
Antonyj Malin (Ramax home virtual PC) <silicium_one@mail.ru>
2020-03-02 17:25:19 +0300

    IMPR:  разделённое API

--- OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/logic/model/IGooglePlacesDiscoverMovieTheatersService.java
+++ OTUSFilmCatalog/app/src/main/java/com/silicium/otusfilmcatalog/logic/model/IGooglePlacesDiscoverMovieTheatersService.java
@@ -15,4 +15,12 @@ public interface IGooglePlacesDiscoverMovieTheatersService {
     @NonNull
     @GET("json?language=ru&rankby=distance&type=movie_theater")
     Call<DiscoverPlacesResultJson> getCinemas(@Query("location") @NonNull String location, @Query("pagetoken") @Nullable String nextPageToken);
+
+    @NonNull
+    @GET("json?language=ru&rankby=distance&type=movie_theater")
+    Call<DiscoverPlacesResultJson> getCinemas(@Query("location") @NonNull String location);
+
+    @NonNull
+    @GET("json?")
+    Call<DiscoverPlacesResultJson> getCinemasNextPage(@Query("pagetoken") @NonNull String nextPageToken);
 }
